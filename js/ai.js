5ba8519c73ad7384a6e10ff6cdd8a73cc99af4a9
LazicDavid <dlazic@deployinc.com>
2015-04-22 00:24:43 +0200

    modified AI logic

--- js/ai.js
+++ js/ai.js
@@ -6,31 +6,82 @@ RESOLVER.AI_MODULE = (function(){
 
         /**
          * AI method.
-         * Plays with predetermined choices when total gets to specific value.
          *
          * @param {Integer} | total - current total amount of coins.
          * @return {Integer}
          */
         playSmart: function(total){
+            var random      = Math.floor((Math.random() * 3) + 1);
+            var breakpoints = this.getBreakpoints(4);
+            var picks       = this.getPicks(3);
+            var map         = this.getMap(breakpoints, picks);
 
-            var o = {
-                '10': 1,
-                '8' : 3,
-                '7' : 2,
-                '6' : 1,
-                '4' : 3,
-                '3' : 2,
-                '2' : 1,
-            };
+            return (map[total]) ? map[total] : random;
+        },
 
-            if(o[total]){
+        /**
+         * Get pick breakpoints.
+         *
+         * @param {Integer} | max - maximum number and value of breakpoints.
+         * @return {Array}
+         */
+        getBreakpoints: function(max){
+            var breakpoints = new Array(max);
+            var breakpoint  = 0;
 
-                return o[total];
+            for(var i = 0; i < max; i++){
 
-            } else{
+                breakpoint += 4;
 
-                return (Math.floor((Math.random() * 3) + 1));
+                breakpoints[i] = breakpoint;
             }
+
+            return breakpoints.reverse();
+        },
+
+        /**
+         * Get picks.
+         *
+         * @param {Integer} | max - maximum number and value of picks.
+         * @return {Array}
+         */
+        getPicks: function(max){
+            var picks = new Array(max);
+
+            for(var i = 0; i < max; i++){
+
+                picks[i] = i + 1;
+            }
+
+            return picks.reverse();
+        },
+
+        /**
+         * Make object map for the breakpoints and picks.
+         *
+         * @param {Array} | breakpoints - pick breakpoints.
+         * @param {Array} | picks       - picks value.
+         * @return {Object}
+         */
+        getMap: function(breakpoints, picks){
+            var o = {};
+
+            for(var bp in breakpoints){
+
+                if(breakpoints.hasOwnProperty(bp)){
+
+                    var index = breakpoints[bp];
+
+                    for(var i = 0; i < picks.length; i++){
+
+                        o[index] = picks[i];
+
+                        index--;
+                    }
+                }
+            }
+
+            return o;
         }
     };
 
